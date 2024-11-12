0102e3d52be6b9c0eb32f5148f12698c172db4d5
M-sharkawy <mostafaeelsharkawy@gmail.com>
2024-11-12 16:09:14 +0200

    init method update kwargs and args

--- models/base_model.py
+++ models/base_model.py
@@ -9,10 +9,18 @@ class BaseModel:
         that defines all common attributes/methods
         for other classes"""
 
-    def __init__(self):
-        self.id = uuid.uuid4()
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        """Initialize a new instance of BaseModel."""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime("%Y-%m-%dT%H:%M:%S.%f")
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = uuid.uuid4()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return (f"[{type(self).__name__}] ({self.id}) {self.__dict__}")
