f1bbb5ed4cd16ccc2e15fb1d5987a02b015b1e50
M-sharkawy <mostafaeelsharkawy@gmail.com>
2024-11-14 10:26:55 +0200

    update consel.py

--- console.py
+++ console.py
@@ -2,13 +2,18 @@
 """module of creating the CMD class"""
 
 import cmd
-
+from models.base_model import BaseModel
+from models import storage
 
 class HBNBCommand(cmd.Cmd):
     """Custom command line interpreter for
     Airbnb project"""
 
     prompt = "(hbnb) "
+
+    classes = {
+        "BaseModel" : BaseModel
+    }
 
     def do_EOF(self, line):
         """Handle EOF to exit the program"""
@@ -22,6 +27,54 @@ class HBNBCommand(cmd.Cmd):
         """pass the emptyline with doing nothing"""
         pass
 
+    def do_create(self,args):
+        """create new instance, 
+        saves it (to the JSON file) and prints the id"""
+        if not args:
+            print("** class name missing **")
+            return
+
+        try:
+            class_name = args.split()[0]
+
+            if class_name not in HBNBCommand.classes:
+                print("** class doesn't exist **")
+                return
+
+            New_Inst = HBNBCommand.classes[class_name]()
+            New_Inst.save()
+            print(New_Inst.id)
+
+        except NameError:
+            print("** class doesn't exist **")
+
+    def do_show(self, args):
+        """Prints the string representation 
+        of an instance based on the class name and id"""
+        cmd_line = args.split()
+
+        if len(cmd_line) == 0:
+            print("** class name missing **")
+            return
+
+        class_name = cmd_line[0]
+
+        if class_name not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+
+        if len(cmd_line) < 2:
+            print("** instance id missing **")
+            return
+
+        inst_id = cmd_line[1]
+        key = f"{class_name}.{inst_id}"
+
+        if key not in storage.all():
+            print("** no instance found **")
+            return
+        else:
+            print(storage.all()[key])
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
