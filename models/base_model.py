2524c3d36f1a62a730121473700ce8c97d77a164
M-sharkawy <mostafasharks021@gmail.com>
2024-11-16 16:01:02 +0200

    kwags function edits in basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -15,14 +15,15 @@ class BaseModel:
 
         if kwargs:
             for key, value in kwargs.items():
-                if key == "created_at":
-                    self.created_at = datetime.strptime(
-                        value, "%Y-%m-%dT%H:%M:%S.%f"
-                        )
-                elif key == "updated_at":
-                    self.updated_at = datetime.strptime(
-                        value, "%Y-%m-%dT%H:%M:%S.%f"
-                        )
+                if key == "updated_at" or key == "created_at":
+                    if key == "updated_at":
+                        self.created_at = datetime.strptime(
+                            value, "%Y-%m-%dT%H:%M:%S.%f"
+                            )
+                    else:
+                        self.updated_at = datetime.strptime(
+                            value, "%Y-%m-%dT%H:%M:%S.%f"
+                            )
                 if key != '__class__':
                     setattr(self, key, value)
         else:
